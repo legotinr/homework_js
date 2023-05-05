1d85cdb33c35e8df0cb5ff6708d9e4fb035fc740
legotinr <legotinr@gmail.com>
2023-05-05 20:27:56 +0300

    done second task

--- liehotin_hw3/script.js
+++ liehotin_hw3/script.js
@@ -1,9 +1,16 @@
-const yearOfBirth = Number(prompt('Please, enter your yaer of birth'))
-let city = prompt('Please, enter your city')
-const sport = prompt('Please, enter your favourite kind of sport')
+const yearOfBirth = prompt('Please, enter your yaer of birth')
+let messageAboutAge = `You are ${2023 - yearOfBirth} years old`
+
+if (yearOfBirth === null) {alert('We are sorry that you did not want to enter your year of birth') 
+messageAboutAge = ''
+}
+
+const city = prompt('Please, enter your city')
 let textAboutCity
-
-
+let messageAboutCity
+if (city === null) {alert('We are sorry that you did not want to enter your city')
+messageAboutCity = ''
+} else {
 switch (city.toLowerCase()) {
     case 'kyiv':
         textAboutCity = 'capital of Ukraine'
@@ -19,5 +26,40 @@ switch (city.toLowerCase()) {
         textAboutCity = city
     break;
 }
-alert(`You are ${2023 - yearOfBirth} years old
-You live in ${textAboutCity}`)
+messageAboutCity = `You live in ${textAboutCity}`
+}
+
+
+const sport = prompt('Please, enter your favourite kind of sport')
+let idol
+let messageAboutSport
+if (sport === null) {alert('We are sorry that you did not want to enter your favourite kind of sport')
+messageAboutSport = ''
+} else {
+
+switch (sport.toLowerCase()) {
+    case 'foorball':
+        idol = 'Cristiano Ronaldo'
+    break;
+    case 'basketball':
+        idol = 'Michael Jordan'
+    break;
+    case 'motorsport':
+        idol = 'Luise Hamilton'
+    break;
+
+    default:
+        idol = null
+    break;
+}
+messageAboutSport = (idol === null) ? '' : `Great! Would you be like ${idol}?`
+}
+
+
+alert(`${messageAboutAge}  
+${messageAboutCity}
+${messageAboutSport}`
+)
+/*alert(`You are ${2023 - yearOfBirth} years old
+You live in ${textAboutCity}
+Great! Would you be like ${idol}?`)*/
