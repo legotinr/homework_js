9fded23164d1e7cb989eb01c4f30c66380701d14
legotinr <legotinr@gmail.com>
2023-05-06 20:29:41 +0300

    updated main script

--- liehotin_hw3/script.js
+++ liehotin_hw3/script.js
@@ -1,62 +1,66 @@
-const yearOfBirth = prompt('Please, enter your year of birth')
-let messageAboutAge = `You are ${2023 - yearOfBirth} years old`
+const yearOfBirth = Number(prompt('Please, enter your year of birth'));
+let messageAboutAge = '';
 
-if (yearOfBirth === null) {alert('We are sorry that you did not want to enter your year of birth') 
-messageAboutAge = ''
-}
 
-const city = prompt('Please, enter your city')
-let textAboutCity
-let messageAboutCity
-if (city === null) {alert('We are sorry that you did not want to enter your city')
-messageAboutCity = ''
+if (!yearOfBirth) { 
+    alert('We are sorry that you did not want to enter your year of birth');
+} else if (yearOfBirth > 1900 && yearOfBirth < 2023) {
+    messageAboutAge = `You are ${2023 - yearOfBirth} years old`;
 } else {
-switch (city.toLowerCase()) {
-    case 'kyiv':
-        textAboutCity = 'capital of Ukraine'
-    break;
-    case 'washington':
-        textAboutCity = 'capital of USA'
-    break;
-    case 'london':
-        textAboutCity = 'capital of Great Britain'
-    break;
-
-    default:
-        textAboutCity = city
-    break;
-}
-messageAboutCity = `You live in ${textAboutCity}`
+    alert('Incorrect year of birth! Please, enter year from 1900 to 2023')
 }
 
-
-const sport = prompt('Please, enter your favourite kind of sport')
-let idol
-let messageAboutSport
-if (sport === null) {alert('We are sorry that you did not want to enter your favourite kind of sport')
-messageAboutSport = ''
+const city = prompt('Please, enter your city');
+let textAboutCity;
+let messageAboutCity = '';
+if (city) {
+    switch (city.toLowerCase()) {
+        case 'kyiv':
+            textAboutCity = 'capital of Ukraine';
+        break;
+        case 'washington':
+            textAboutCity = 'capital of USA';
+        break;
+        case 'london':
+            textAboutCity = 'capital of Great Britain';
+        break;
+    
+        default:
+            textAboutCity = city;
+        break;
+    }
+    messageAboutCity = `You live in ${textAboutCity}`;
 } else {
-
-switch (sport.toLowerCase()) {
-    case 'football':
-        idol = 'Cristiano Ronaldo'
-    break;
-    case 'basketball':
-        idol = 'Michael Jordan'
-    break;
-    case 'motorsport':
-        idol = 'Luise Hamilton'
-    break;
-
-    default:
-        idol = null
-    break;
-}
-messageAboutSport = (idol === null) ? '' : `Great! Would you be like ${idol}?`
+    alert('We are sorry that you did not want to enter your city');
 }
 
 
-alert(`${messageAboutAge}  
+const sport = prompt('Please, enter your favourite kind of sport');
+let idol;
+let messageAboutSport = '';
+if (sport) {
+    switch (sport.toLowerCase()) {
+        case 'football':
+            idol = 'Cristiano Ronaldo';
+        break;
+        case 'basketball':
+            idol = 'Michael Jordan';
+        break;
+        case 'motorsport':
+            idol = 'Luise Hamilton';
+        break;
+    
+        default:
+            idol = null;
+        break;
+    }
+    if (idol != null) messageAboutSport = `Great! Would you be like ${idol}?`;
+} else {
+alert('We are sorry that you did not want to enter your favourite kind of sport');
+}
+
+alert(
+`${messageAboutAge}  
 ${messageAboutCity}
 ${messageAboutSport}`
-)
+);
