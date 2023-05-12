e8d773c52eb3c5b6e5028ab09fb80929c72a6221
legotinr <legotinr@gmail.com>
2023-05-12 15:58:29 +0300

    create files on folder hw5 and hw6, in hw5 completed main script

---
+++ liehotin_hw5/script.js
@@ -0,0 +1,12 @@
+const arrLength = Number(prompt('Enter array length(more or equal 4)'));
+if (arrLength && arrLength >= 4) {
+    const arr = [];
+    for (i = 0; i < arrLength; i++) {
+        arr[i] = prompt(`Enter arr[${i}] element`);
+        console.log(arr);
+    }
+arr.splice(1, 3);
+alert(arr);
+} else {
+    alert('Incorrect array length. Enter only number more or equal 4');
+}
